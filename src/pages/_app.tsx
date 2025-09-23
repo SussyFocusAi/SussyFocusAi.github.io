56297e63bd6e1101eeaa2dcd77ff4934dd4cf851
raymond <toronto4raymond2010@gmai.com>
2025-09-23 04:25:15 -0400

    remove next export for NextAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
 // src/pages/_app.tsx
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function MyApp({ 
   Component, 
   pageProps: { session, ...pageProps } 
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      
+      <SpeedInsights />
       <Component {...pageProps} />
     </SessionProvider>
   );
