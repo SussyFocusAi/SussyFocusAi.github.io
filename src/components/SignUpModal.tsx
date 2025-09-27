c48292338ca94d813c09a41c35f490a9df600601
raymond <toronto4raymond2010@gmai.com>
2025-09-27 10:10:42 -0400

    updating files

--- src/components/SignUpModal.tsx
+++ src/components/SignUpModal.tsx
@@ -128,7 +128,7 @@ const handleSubmit = async (e: React.FormEvent) => {
   setErrors({});
 
   try {
-    const res = await fetch('/api/signup', {
+    const res = await fetch('/api/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
